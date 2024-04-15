a6f812def36e62fc1e62622331989c1122859b7a
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-04-15 17:21:27 -0500

    Commit
    Clase ProductoTela

---
+++ ConsoleApp1/ConsoleApp1/ProductoTela.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ProductoTela : Producto
+    {
+        public string Tamaño { get; set; }
+        public string Material { get; set; }
+        public string Color { get; set; }
+
+        public override string Describir()
+        {
+            return $"Producto de tela: {Nombre}, Tamaño: {Tamaño}, Material: {Material}, Color: {Color}, Precio: {Precio}";
+        }
+    }
+}
