19b796e0739b3e11193012acc40d1216b7ac0860
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-04-15 17:35:09 -0500

    Create CarritoCompras.cs

---
+++ ConsoleApp1/ConsoleApp1/CarritoCompras.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class CarritoCompras 
+    {
+        private List<Producto> productos = new List<Producto>();
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public double CalcularPrecioTotal()
+        {
+            double precioTotal = 0;
+            foreach(var producto in productos)
+            {
+                precioTotal += producto.Precio;
+            }
+            return precioTotal;
+        }
+
+        public void MostrarContenido()
+        {
+            Console.WriteLine("Contenido del carrito:");
+            foreach (var producto in productos)
+            {
+                Console.WriteLine($"{producto.Nombre} : ${producto.Precio}");
+            }
+
+        }
+
+    }
+}
