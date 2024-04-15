78513784eec83cb0ae9e1882596cb694db335310
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-04-15 17:18:40 -0500

    Commit
    Clase Producto

---
+++ ConsoleApp1/ConsoleApp1/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Producto
+    {
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+
+        public virtual string Describir()
+        {
+            return $"Producto: {Nombre}, Precio: {Precio}";
+        }
+
+    }
+}
