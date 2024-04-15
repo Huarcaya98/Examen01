d245ed26a58a8b41058228c76f1b84f469322fcf
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-04-15 17:23:34 -0500

    Create ProductoArcilla.cs
    Clase ProductoArcilla

---
+++ ConsoleApp1/ConsoleApp1/ProductoArcilla.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ProductoArcilla : Producto
+    {
+        public double Peso { get; set; }
+        public string Tamaño { get; set; }
+        public string Color { get; set; }
+
+        public override string Describir()
+        {
+            return $"Producto de arcilla: {Nombre}, Peso: {Peso} kg, Tamaño: {Tamaño}, Color: {Color}, Precio: {Precio}";
+        }
+    }
+}
